41665625a1056c89ad0e8a5716a1b4179ed13222
krishna13122000 <sai.mamidala1312@gmail.com>
2024-05-25 19:27:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+document.getElementById('open').addEventListener('click',()=>{
+    document.getElementById('navLinks').style.right = '0';
+})
+document.getElementById('close').addEventListener('click',()=>{
+    document.getElementById('navLinks').style.right = '-200px';
+})
